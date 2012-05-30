70f987818051576b96d68539dd673d1f760fbd72
Chris Jackson <jackson.chris@gmail.com>
2012-05-30 17:33:29 -0400

    Inital commit.

---
+++ SDR Frequency Manager/Form1.designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lstUser = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.rdoNFM = New System.Windows.Forms.RadioButton()
+        Me.rdoWFM = New System.Windows.Forms.RadioButton()
+        Me.rdoAM = New System.Windows.Forms.RadioButton()
+        Me.rdoDSB = New System.Windows.Forms.RadioButton()
+        Me.rdoLSB = New System.Windows.Forms.RadioButton()
+        Me.rdoUSB = New System.Windows.Forms.RadioButton()
+        Me.rdoCWL = New System.Windows.Forms.RadioButton()
+        Me.rdoCWU = New System.Windows.Forms.RadioButton()
+        Me.txtFreq = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lstBand = New System.Windows.Forms.ListBox()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtName.Location = New System.Drawing.Point(396, 150)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(156, 20)
+        Me.txtName.TabIndex = 11
+        '
+        'lstUser
+        '
+        Me.lstUser.FormattingEnabled = True
+        Me.lstUser.Location = New System.Drawing.Point(155, 30)
+        Me.lstUser.Name = "lstUser"
+        Me.lstUser.Size = New System.Drawing.Size(225, 225)
+        Me.lstUser.Sorted = True
+        Me.lstUser.TabIndex = 1
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(396, 232)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 13
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'rdoNFM
+        '
+        Me.rdoNFM.AutoSize = True
+        Me.rdoNFM.Location = New System.Drawing.Point(399, 31)
+        Me.rdoNFM.Name = "rdoNFM"
+        Me.rdoNFM.Size = New System.Drawing.Size(77, 17)
+        Me.rdoNFM.TabIndex = 3
+        Me.rdoNFM.TabStop = True
+        Me.rdoNFM.Text = "Narrow FM"
+        Me.rdoNFM.UseVisualStyleBackColor = True
+        '
+        'rdoWFM
+        '
+        Me.rdoWFM.AutoSize = True
+        Me.rdoWFM.Location = New System.Drawing.Point(399, 54)
+        Me.rdoWFM.Name = "rdoWFM"
+        Me.rdoWFM.Size = New System.Drawing.Size(68, 17)
+        Me.rdoWFM.TabIndex = 4
+        Me.rdoWFM.TabStop = True
+        Me.rdoWFM.Text = "Wide FM"
+        Me.rdoWFM.UseVisualStyleBackColor = True
+        '
+        'rdoAM
+        '
+        Me.rdoAM.AutoSize = True
+        Me.rdoAM.Location = New System.Drawing.Point(399, 77)
+        Me.rdoAM.Name = "rdoAM"
+        Me.rdoAM.Size = New System.Drawing.Size(41, 17)
+        Me.rdoAM.TabIndex = 5
+        Me.rdoAM.TabStop = True
+        Me.rdoAM.Text = "AM"
+        Me.rdoAM.UseVisualStyleBackColor = True
+        '
+        'rdoDSB
+        '
+        Me.rdoDSB.AutoSize = True
+        Me.rdoDSB.Location = New System.Drawing.Point(399, 100)
+        Me.rdoDSB.Name = "rdoDSB"
+        Me.rdoDSB.Size = New System.Drawing.Size(47, 17)
+        Me.rdoDSB.TabIndex = 6
+        Me.rdoDSB.TabStop = True
+        Me.rdoDSB.Text = "DSB"
+        Me.rdoDSB.UseVisualStyleBackColor = True
+        '
+        'rdoLSB
+        '
+        Me.rdoLSB.AutoSize = True
+        Me.rdoLSB.Location = New System.Drawing.Point(496, 31)
+        Me.rdoLSB.Name = "rdoLSB"
+        Me.rdoLSB.Size = New System.Drawing.Size(45, 17)
+        Me.rdoLSB.TabIndex = 7
+        Me.rdoLSB.TabStop = True
+        Me.rdoLSB.Text = "LSB"
+        Me.rdoLSB.UseVisualStyleBackColor = True
+        '
+        'rdoUSB
+        '
+        Me.rdoUSB.AutoSize = True
+        Me.rdoUSB.Location = New System.Drawing.Point(496, 54)
+        Me.rdoUSB.Name = "rdoUSB"
+        Me.rdoUSB.Size = New System.Drawing.Size(47, 17)
+        Me.rdoUSB.TabIndex = 8
+        Me.rdoUSB.TabStop = True
+        Me.rdoUSB.Text = "USB"
+        Me.rdoUSB.UseVisualStyleBackColor = True
+        '
+        'rdoCWL
+        '
+        Me.rdoCWL.AutoSize = True
+        Me.rdoCWL.Location = New System.Drawing.Point(496, 77)
+        Me.rdoCWL.Name = "rdoCWL"
+        Me.rdoCWL.Size = New System.Drawing.Size(52, 17)
+        Me.rdoCWL.TabIndex = 9
+        Me.rdoCWL.TabStop = True
+        Me.rdoCWL.Text = "CW-L"
+        Me.rdoCWL.UseVisualStyleBackColor = True
+        '
+        'rdoCWU
+        '
+        Me.rdoCWU.AutoSize = True
+        Me.rdoCWU.Location = New System.Drawing.Point(496, 100)
+        Me.rdoCWU.Name = "rdoCWU"
+        Me.rdoCWU.Size = New System.Drawing.Size(54, 17)
+        Me.rdoCWU.TabIndex = 10
+        Me.rdoCWU.TabStop = True
+        Me.rdoCWU.Text = "CW-U"
+        Me.rdoCWU.UseVisualStyleBackColor = True
+        '
+        'txtFreq
+        '
+        Me.txtFreq.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtFreq.Location = New System.Drawing.Point(396, 200)
+        Me.txtFreq.Name = "txtFreq"
+        Me.txtFreq.Size = New System.Drawing.Size(156, 20)
+        Me.txtFreq.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(397, 131)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Label"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(397, 184)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 13)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Frequency"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 14)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 21
+        Me.Label5.Text = "Bands"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(152, 14)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(90, 13)
+        Me.Label6.TabIndex = 22
+        Me.Label6.Text = "User Frequencies"
+        '
+        'lstBand
+        '
+        Me.lstBand.FormattingEnabled = True
+        Me.lstBand.Location = New System.Drawing.Point(12, 30)
+        Me.lstBand.Name = "lstBand"
+        Me.lstBand.Size = New System.Drawing.Size(127, 225)
+        Me.lstBand.Sorted = True
+        Me.lstBand.TabIndex = 0
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(477, 232)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 14
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(304, 12)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 15)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(573, 278)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.lstBand)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtFreq)
+        Me.Controls.Add(Me.rdoCWU)
+        Me.Controls.Add(Me.rdoCWL)
+        Me.Controls.Add(Me.rdoUSB)
+        Me.Controls.Add(Me.rdoLSB)
+        Me.Controls.Add(Me.rdoDSB)
+        Me.Controls.Add(Me.rdoAM)
+        Me.Controls.Add(Me.rdoWFM)
+        Me.Controls.Add(Me.rdoNFM)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstUser)
+        Me.Controls.Add(Me.txtName)
+        Me.Name = "Form1"
+        Me.Text = "SDRSharp Frequency Manager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents lstUser As System.Windows.Forms.ListBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents rdoNFM As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoWFM As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoAM As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoDSB As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoLSB As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoUSB As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoCWL As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoCWU As System.Windows.Forms.RadioButton
+    Friend WithEvents txtFreq As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents lstBand As System.Windows.Forms.ListBox
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+
+End Class
---
+++ SDR Frequency Manager/Form1.vb
@@ -0,0 +1,320 @@
+﻿'Copyright (C) 2012 christopherx
+
+'Permission is hereby granted, free of charge, to any person obtaining a copy 
+'of this software and associated documentation files (the "Software"), to deal 
+'in the Software without restriction, including without limitation the rights 
+'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+'of the Software, and to permit persons to whom the Software is furnished to do 
+'so, subject to the following conditions:
+
+'The above copyright notice and this permission notice shall be included in all
+'copies or substantial portions of the Software.
+
+'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+'INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+'PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+'HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+'OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+'SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Threading
+
+Public Class Form1
+    'todo
+    'squelch for NFM
+    'scanner
+
+    Const WM_SETTEXT As Int32 = 12
+    Const WM_KEYDOWN As Int32 = 256
+    Const WM_KEYUP As Int32 = 257
+
+    Public childcount As Int32
+    Public cfhwnd As Int32
+    Public fhwnd As Int32
+    Public nfmhwnd As Int32
+    Public amhwnd As Int32
+    Public wfmhwnd As Int32
+    Public lsbhwnd As Int32
+    Public usbhwnd As Int32
+    Public dsbhwnd As Int32
+    Public cwlhwnd As Int32
+    Public cwuhwnd As Int32
+
+    Private Declare Function SendMessage Lib "user32" Alias "SendMessageA" (ByVal hWnd As Int32, _
+                                                                            ByVal wMsg As Int32, _
+                                                                            ByVal wParam As Int32, _
+                                                                            ByVal lParam As String) As Int32
+
+    Private Declare Function PostMessage Lib "user32" Alias "PostMessageA" (ByVal hWnd As Int32, _
+                                                                            ByVal wMsg As Int32, _
+                                                                            ByVal wParam As Int32, _
+                                                                            ByVal lParam As String) As Int32
+
+    Private Delegate Function EnumChildProc(ByVal hwnd As IntPtr, ByVal lParam As IntPtr) As Boolean
+
+    <DllImport("user32")> _
+    Private Shared Function EnumChildWindows(ByVal hWndParent As IntPtr, _
+                                         ByVal lpEnumProc As EnumChildProc, _
+                                         ByVal lParam As IntPtr) As Boolean
+    End Function
+
+
+    Private Sub EnumChildWindows(ByVal parentWindow As IntPtr)
+
+        EnumChildWindows(parentWindow, AddressOf ProcessChildWindow, IntPtr.Zero)
+
+    End Sub
+
+
+    Private Function ProcessChildWindow(ByVal hwnd As IntPtr, ByVal lParam As IntPtr) As Boolean
+
+        childcount = childcount + 1
+
+        'DEBUG find childcount for particular hwnd
+        'If hwnd = 1511440 Then
+        '    MsgBox(childcount)
+        'End If
+
+        Select Case childcount
+            Case 12
+                nfmhwnd = hwnd
+            Case 20
+                amhwnd = hwnd
+            Case 17
+                wfmhwnd = hwnd
+            Case 22
+                lsbhwnd = hwnd
+            Case 24
+                usbhwnd = hwnd
+            Case 13
+                dsbhwnd = hwnd
+            Case 11
+                cwlhwnd = hwnd
+            Case 10
+                cwuhwnd = hwnd
+            Case 16
+                fhwnd = hwnd
+            Case 45
+                cfhwnd = hwnd
+        End Select
+
+        Return True
+
+    End Function
+
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        'load settings
+        Dim strTmp As String
+        strTmp = My.Settings.strUsr
+
+        If strTmp <> Nothing Then
+            Dim strArray() As String
+            strArray = Split(strTmp, ";")
+            Dim i As Integer
+            For i = 0 To UBound(strArray) - 1
+                lstUser.Items.Add(strArray(i))
+            Next
+        End If
+
+        'get sdrsharp hwnd
+        Dim hwnd As Int32
+        Dim pIndex As Int32 = 0
+        For Each p As System.Diagnostics.Process In System.Diagnostics.Process.GetProcessesByName("SDRSharp")
+            pIndex += 1
+            If pIndex = 1 Then hwnd = p.MainWindowHandle.ToInt32
+        Next
+
+        If hwnd = Nothing Then
+            MsgBox("Please launch and 'Play' SDRSharp prior to launching SDRSharp Frequency Manager")
+            Exit Sub
+        End If
+
+        'get child hwnds
+        childcount = 0
+        EnumChildWindows(hwnd)
+
+        'add bands center freq
+        'should probably refactor, this is lazy
+        lstBand.Items.Add("126,000,000")
+        '2m
+        lstBand.Items.Add("145,000,000")
+        lstBand.Items.Add("147,000,000")
+        '1.25m
+        lstBand.Items.Add("221,000,000")
+        lstBand.Items.Add("223,000,000")
+        lstBand.Items.Add("225,000,000")
+        '70cm
+        lstBand.Items.Add("421,000,000")
+        lstBand.Items.Add("423,000,000")
+        lstBand.Items.Add("425,000,000")
+        lstBand.Items.Add("427,000,000")
+        lstBand.Items.Add("429,000,000")
+        lstBand.Items.Add("431,000,000")
+        lstBand.Items.Add("433,000,000")
+        lstBand.Items.Add("435,000,000")
+        lstBand.Items.Add("437,000,000")
+        lstBand.Items.Add("439,000,000")
+        lstBand.Items.Add("441,000,000")
+        lstBand.Items.Add("443,000,000")
+        lstBand.Items.Add("445,000,000")
+        lstBand.Items.Add("447,000,000")
+        lstBand.Items.Add("449,000,000")
+        '33cm
+        lstBand.Items.Add("903,000,000")
+        lstBand.Items.Add("905,000,000")
+        lstBand.Items.Add("907,000,000")
+        lstBand.Items.Add("909,000,000")
+        lstBand.Items.Add("911,000,000")
+        lstBand.Items.Add("913,000,000")
+        lstBand.Items.Add("915,000,000")
+        lstBand.Items.Add("917,000,000")
+        lstBand.Items.Add("919,000,000")
+        lstBand.Items.Add("921,000,000")
+        lstBand.Items.Add("923,000,000")
+        lstBand.Items.Add("925,000,000")
+        lstBand.Items.Add("927,000,000")
+
+    End Sub
+
+
+    Private Sub lstBand_DoubleClick(sender As Object, e As System.EventArgs) Handles lstBand.DoubleClick
+
+        Dim cf As String = lstBand.Items(lstBand.SelectedIndex)
+
+        SendMessage(cfhwnd, WM_SETTEXT, 0, cf)
+        PostMessage(cfhwnd, WM_KEYDOWN, Keys.Enter, Nothing)
+        PostMessage(cfhwnd, WM_KEYUP, Keys.Enter, Nothing)
+
+        Thread.Sleep(100)
+
+        'lazy
+        PostMessage(nfmhwnd, WM_KEYDOWN, Keys.Space, Nothing)
+        PostMessage(nfmhwnd, WM_KEYUP, Keys.Space, Nothing)
+
+    End Sub
+
+
+    Private Sub lstUser_DoubleClick(sender As Object, e As System.EventArgs) Handles lstUser.DoubleClick
+
+        Dim f As String = Split(lstUser.Items(lstUser.SelectedIndex), " -- ")(1)
+        Dim cf As String = f - 20000
+
+        Dim md As String = Split(lstUser.Items(lstUser.SelectedIndex), " -- ")(2)
+
+        Dim mdhwnd As Int32
+
+        Select Case md
+            Case "NFM"
+                mdhwnd = nfmhwnd
+            Case "AM"
+                mdhwnd = amhwnd
+            Case "WFM"
+                mdhwnd = wfmhwnd
+            Case "LSB"
+                mdhwnd = lsbhwnd
+            Case "USB"
+                mdhwnd = usbhwnd
+            Case "DSB"
+                mdhwnd = dsbhwnd
+            Case "CWL"
+                mdhwnd = cwlhwnd
+            Case "CWU"
+                mdhwnd = cwuhwnd
+        End Select
+
+        'center freq
+        SendMessage(cfhwnd, WM_SETTEXT, 0, cf)
+        PostMessage(cfhwnd, WM_KEYDOWN, Keys.Enter, Nothing)
+        PostMessage(cfhwnd, WM_KEYUP, Keys.Enter, Nothing)
+
+        Thread.Sleep(100)
+
+        'freq
+        SendMessage(fhwnd, WM_SETTEXT, 0, f)
+        PostMessage(fhwnd, WM_KEYDOWN, Keys.Enter, Nothing)
+        PostMessage(fhwnd, WM_KEYUP, Keys.Enter, Nothing)
+
+        Thread.Sleep(100)
+
+        'mode
+        PostMessage(mdhwnd, WM_KEYDOWN, Keys.Space, Nothing)
+        PostMessage(mdhwnd, WM_KEYUP, Keys.Space, Nothing)
+
+    End Sub
+
+
+    Private Sub btnSave_Click(sender As System.Object, e As System.EventArgs) Handles btnSave.Click
+
+        If MessageBox.Show("Are you sure you want to save?", "Save", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+            Dim i As Integer
+            Dim strTmp As String
+            strTmp = ""
+
+            For i = 0 To (lstUser.Items.Count - 1)
+                strTmp = strTmp & lstUser.Items(i).ToString & ";"
+            Next
+
+            My.Settings.strUsr = strTmp
+            My.Settings.Save()
+            MsgBox("Settings saved.")
+        Else
+            MsgBox("Save aborted.")
+        End If
+
+    End Sub
+
+
+    Private Sub btnAdd_Click(sender As System.Object, e As System.EventArgs) Handles btnAdd.Click
+
+        If txtName.Text = "" Or txtFreq.Text = "" Then
+            MsgBox("Please enter both a Label and Frequency before adding to User List.")
+            Exit Sub
+        End If
+
+        Dim md As String
+        md = "NFM"
+        For Each ctl As Control In Me.Controls
+            If TypeOf ctl Is RadioButton Then
+                If CType(ctl, RadioButton).Checked = True Then
+                    md = Split(ctl.Name, "rdo")(1)
+                    Exit For
+                End If
+            End If
+        Next
+
+        lstUser.Items.Add(txtName.Text & " -- " & txtFreq.Text & " -- " & md)
+
+    End Sub
+
+
+    Private Sub btnDelete_Click(sender As System.Object, e As System.EventArgs) Handles btnDelete.Click
+
+        lstUser.Items.RemoveAt(lstUser.SelectedIndex)
+
+    End Sub
+
+
+    Private Sub txtFreq_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles txtFreq.KeyDown
+
+        If e.KeyCode = Keys.Enter Then
+            e.SuppressKeyPress = True
+            btnAdd_Click(sender, e)
+        End If
+
+    End Sub
+
+
+    Private Sub Form1_Resize(sender As Object, e As System.EventArgs) Handles Me.Resize
+
+        lstUser.Height = Me.Height - 75
+        lstBand.Height = Me.Height - 75
+
+    End Sub
+
+
+End Class
